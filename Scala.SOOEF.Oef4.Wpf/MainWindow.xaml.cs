488d8957d79a1657f0f6a8a4f5c4ce75ca559d2e
jandedeurwaerder-howest <jan.de.deurwaerder@howest.be>
2023-05-16 08:53:58 +0200

    Add project files.

---
+++ Scala.SOOEF.Oef4.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scala.SOOEF.Oef4.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LstVakantiehuizen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnNieuw_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnWijzig_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnVerwijder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnBewaren_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnnuleren_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
